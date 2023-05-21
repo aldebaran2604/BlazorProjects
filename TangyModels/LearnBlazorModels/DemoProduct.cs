2d189129ad0972d1748a4a723cbf68eba8486d41
Luis Jose Padilla Benitez <luis.padilla.26041987@gmail.com>
2023-05-20 21:49:51 -0600

    Section 3: 3 Blazor - Data and Property Binding 16. One way Data Binding 17. 3 Two Way Data Binding 18. 4 Assignment 1 - Data Binding 19. 5 Assignment 1  Solution - Data Binding 20. 6 Dropdowns and Data Binding 21. 7 Organizing Individual Product Playground

---
+++ TangyModels/LearnBlazorModels/DemoProduct.cs
@@ -0,0 +1,11 @@
+﻿namespace TangyModels.LearnBlazorModels;
+
+public class DemoProduct
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public decimal Price { get; set; }
+    public bool IsActive { get; set; }
+
+    public IEnumerable<DemoProductProp> ProductProperties { get; set; }
+}
---
+++ TangyModels/LearnBlazorModels/DemoProductProp.cs
@@ -0,0 +1,8 @@
+﻿namespace TangyModels.LearnBlazorModels;
+
+public class DemoProductProp
+{
+    public int Id { get; set; }
+    public string Key { get; set; } = null!;
+    public string Value { get; set; } = null!;
+}
